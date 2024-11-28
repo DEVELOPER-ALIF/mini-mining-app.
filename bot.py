37ae561b8b1ff2633ebb821666d055b7e7f4ef12
Md Alif <mynameismdalif@gmail.com>
2024-11-28 08:59:01 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,23 @@
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-# Replace this with your bot's API token
 API_TOKEN = "7605669588:AAHP6RNFa2M0R1fDSHAJY3oQnL-x4G3PXWU"
-WEB_APP_URL = "https://developer-alif.github.io/mini-mining-app./"  # Replace with your Web App URL
-
 bot = telebot.TeleBot(API_TOKEN)
 
 # Start command
 @bot.message_handler(commands=["start"])
 def start(message):
-    user_id = message.chat.id
-    # Create a WebAppInfo object with your app URL
-    web_app_info = WebAppInfo(url=WEB_APP_URL)
-    # Inline keyboard with a Play button
-    keyboard = InlineKeyboardMarkup()
-    play_button = InlineKeyboardButton("🎮 Play", web_app=web_app_info)  # Use WebAppInfo here
-    keyboard.add(play_button)
+    # Send the Web App link
     bot.send_message(
-        user_id,
-        "Welcome to the Mini Mining Bot! Click the button below to start mining:",
-        reply_markup=keyboard
+        message.chat.id,
+        "Welcome to the Coin Mining Game! Tap below to start mining.",
+        reply_markup=telebot.types.ReplyKeyboardMarkup(
+            one_time_keyboard=True, resize_keyboard=True
+        ).add(
+            telebot.types.KeyboardButton(
+                "Play",
+                web_app=telebot.types.WebAppInfo(url="https://developer-alif.github.io/mini-mining-app./")  # Link to your hosted mini app
+            )
+        )
     )
 
-# Run the bot
 bot.polling()
